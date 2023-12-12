884136847ce8d95fdf7c50051c8a17242037128c
SandXs <archies559@gmail.com>
2023-12-12 18:20:01 +0100

    default_values.php updated and groups added

--- default_values.php
+++ default_values.php
@@ -1,16 +1,9 @@
 <?php
 
 //ticket types
-$ticket_type_arr[] = array();
-$ticket_type_arr[0] = "Account aanvraag";
-$ticket_type_arr[1] = "Test aanvraag";
-$ticket_type_arr[2] = "Q&A";
-$ticket_type_arr[3] = "Andere";
-
-//ticket priority
-$ticket_priority_arr[] = array();
-$ticket_priority_arr[0] = array("Laag","color");
-$ticket_priority_arr[1] = array("Middel","color");
-$ticket_priority_arr[2] = array("Hoog","color");
+$user_group_arr[] = array();
+$user_group_arr[0] = "Gebruiker";
+$user_group_arr[1] = "Werknemer";
+$user_group_arr[2] = "Admin";
 
 ?>
